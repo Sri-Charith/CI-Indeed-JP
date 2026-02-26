39766942662f483f3b2f669b79484ea41e01417c
Sri Charith Rampelli <charithrampelli2005@gmail.com>
2026-02-26 22:40:02 +0530

    axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://ci-jp.onrender.com/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
-// Add a request interceptor to include the token
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
+// Attach JWT token automatically
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default api;
